b2a58741f57347a43858498cc007d59a0366b03d
Tashrif Antora <125790353+tashrifantora@users.noreply.github.com>
2023-08-18 20:07:25 +0600

    done

--- JS/area.calculator.js
+++ JS/area.calculator.js
@@ -1,28 +1,145 @@
 // Traingle Area
 function calculateTraingleArea() {
-    // Traingle base
-    const baseField = document.getElementById('traingle-base').value;
-    const base = parseFloat(baseField);
+    // Functional way
+    const base = getInputValue('traingle-base');
+    const hight = getInputValue('traingle-hight');
 
-    // Traingle hight
-    const hightField = document.getElementById('traingle-hight').value;
-    const hight = parseFloat(hightField);
+
+    // Validation
+    if (isNaN(base) || isNaN(hight)) {
+        alert('Please insert a number');
+        return;
+    }
 
     const area = 0.5 * base * hight;
-    document.getElementById('traingle-area').innerText = area;
+    setTextFieldText('traingle-area', area);
+
+    // Add to calculation
+    addToCalculateEntry('traingle', area);
 }
 
 // Rectraingle Area
 function calculateRectraingleArea() {
-    // rectraingle width
-    const widthField = document.getElementById('rectangle-width').value;
-    const width = parseFloat(widthField);
+    // Funtional way
+    const width = getInputValue('rectangle-width');
+    const length = getInputValue('rectangle-length');
 
-    // rectraingle length
-    const lengthField = document.getElementById('rectangle-length').value;
-    const length = parseFloat(lengthField);
 
-    // Result + WSet Text
+    // Validation
+    if (isNaN(width) || isNaN(length)) {
+        alert('Please insert a number');
+        return;
+    }
+
     const area = width * length;
-    document.getElementById('rectangle-area').innerText = area;
+    setTextFieldText('rectangle-area', area);
+
+    // Add to calculation
+    addToCalculateEntry('rectraingle', area);
+}
+
+
+// Parallelogram Area 
+function calculateParallelogramArea() {
+    const base = getInputValue('parallelogram-base');
+    const hight = getInputValue('parallelogram-hight');
+
+
+    // Validation
+    if (isNaN(base) || isNaN(hight)) {
+        alert('Please insert a number');
+        return;
+    }
+
+    const area = base * hight;
+    setTextFieldText('parallelogram-area', area);
+
+    // Add to calculation
+    addToCalculateEntry('parallelogram', area);
+}
+
+// Ellipse Area
+function calculateEllipseArea() {
+    const majorRadius = getInputValue('ellipse-major-radius');
+    const minorRadius = getInputValue('ellipse-minor-radius');
+
+    // Validation
+    if (isNaN(majorRadius) || isNaN(minorRadius)) {
+        alert('Please insert a number');
+        return;
+    }
+
+    const area = 3.14 * majorRadius * minorRadius;
+    const areaTwoDecimal = area.toFixed(2);
+    setTextFieldText("ellipse-area", areaTwoDecimal)
+
+    // Add to calculation
+    addToCalculateEntry('Ellipse', areaTwoDecimal);
+}
+
+// Pentagon Area
+function calculatePentagonArea() {
+    const perimeter = getInputValue("pentagon-perimeter");
+    const apothem = getInputValue("pentagon-apothem");
+
+    // Validation
+    if (isNaN(perimeter) || isNaN(apothem)) {
+        alert('Please insert a number');
+        return;
+    }
+
+    const area = 0.5 * perimeter * apothem;
+    setTextFieldText('pentagon-area', area);
+
+    // Add to calculation
+    addToCalculateEntry('Pentagon', area);
+}
+
+// Rhombus Area
+function calculateRhombusArea() {
+    const base = getInputValue('rhombus-base');
+    const hight = getInputValue('rhombus-hight');
+
+
+    // Validation
+    if (isNaN(base) || isNaN(hight)) {
+        alert('Please insert a number');
+        return;
+    }
+
+    const area = 0.5 * base * hight;
+    setTextFieldText('rhombus-area', area);
+
+    // Add to calculation
+    addToCalculateEntry('Rhombus', area);
+}
+
+// Function for getting input value
+function getInputValue(fieldId) {
+    const inputField = document.getElementById(fieldId).value;
+    const value = parseFloat(inputField);
+    return value
+}
+
+
+// Reuseable set value 
+function setTextFieldText(elementId, area) {
+    document.getElementById(elementId).innerText = area;
+}
+
+
+/* <==============================>
+     Add to Calculation entry
+<=================================>
+*/
+function addToCalculateEntry(areaType, area) {
+    console.log(areaType + ' ' + area);
+    const calculatonEntry = document.getElementById('calculation-entry');
+
+    const count = calculatonEntry.childElementCount;
+
+    const p = document.createElement('p');
+    p.classList.add('my-4')
+    p.innerHTML = `${count + 1}. ${areaType} ${area} cm<sup>2</sup> <button class="btn btn-sm btn-primary">Convert</button>`
+    console.log(calculatonEntry.appendChild(p));
 }
